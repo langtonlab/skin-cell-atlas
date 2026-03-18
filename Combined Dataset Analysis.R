d8b684e69194d12e12f6f2cffa04360ed0509cb6
langtonlab <bezaleel.mambwe@manchester.ac.uk>
2026-03-18 10:45:56 +0000

    Update print statement from 'Hello' to 'Goodbye'

--- Combined Dataset Analysis.R
+++ Combined Dataset Analysis.R
@@ -17,7 +17,7 @@ library(extrafont)
 # Load Xenium files
 # -----------------------------
 paths <- c(
-"insert paths here"
+"INSERT CELL FEATURE MATRIX PATHS"
 )
 
 # Read Xenium files
@@ -38,19 +38,17 @@ process_xenium_sample <- function(path, sample_name) {
   return(obj)
 }
 
+sample_names <- c("D1921", "D1923", "D1924", "D1925", "D1922")
 
 # Read + merge all samples in one go
 objs <- lapply(seq_along(paths), function(i) {
-  s_name <- paste0("G2_S", i)
-  obj <- process_xenium_sample(paths[i], s_name)
+  s_name <- sample_names[i]
+   obj <- process_xenium_sample(paths[i], s_name)
   obj <- RenameCells(obj, add.cell.id = s_name)
   return (obj)
 })
 
 all_combined_data <- Reduce(merge, objs)
-
-message("Merged cells: ", ncol(all_combined_data))
-
 
 # ---------------------------
 # Preprocessing: SCTransform
@@ -245,19 +243,6 @@ DimPlot(
   label.size = 4,
   cols = custom_colors
 ) + theme(legend.position = "right")
-
-
-# # ---------------------------
-# # 
-# # ---------------------------
-
-# head(all_combined_data@meta.data[, c("seurat_clusters", "CellType")])
-
-# fibro_clusters <- all_combined_data@meta.data %>%
-#   filter(CellType == "Fibroblasts") %>%
-#   distinct(seurat_clusters)
-# fibro_clusters
-# nrow(fibro_clusters)
 
 # ---------------------------
 # Cell Proportions
@@ -1116,3 +1101,353 @@ ggsave(
   height = 6,
   units = "cm"
 )
+
+#------------------------------------
+# Single Individual Analysis for Representative Images
+#------------------------------------
+
+d1922 <- subset(all_combined_data, subset = sample == "D1922")
+
+DimPlot(d1922, reduction = "umap", group.by = "CellType", label = FALSE)
+
+# -----------------------------
+# Hightlight cell clusters spatially
+# -----------------------------
+
+seg_file <- "INSERT cell_boundaries.parquet PATH"
+
+seg_data <- read_parquet(seg_file)
+
+seg_sf <- seg_data %>%
+  group_by(cell_id, label_id) %>%
+  summarise(
+    geometry = st_sfc(
+      st_polygon(list(
+        rbind(
+          cbind(vertex_x, vertex_y),
+          cbind(vertex_x[1], vertex_y[1])
+        )
+      ))
+    ),
+    .groups = "drop"
+  ) %>%
+  st_as_sf()
+
+# Add cell_id column based on rownames
+cell_annotations <- d1922@meta.data %>%
+  mutate(cell_id = sub("^sample_D1922_", "", rownames(.))) %>%
+  select(cell_id, CellType)
+
+# Now join with the polygons
+seg_annot <- seg_sf %>%
+  left_join(cell_annotations, by = "cell_id")
+
+# Create robust fill_group for Immune highlighting
+seg_annot <- seg_annot %>%
+  mutate(
+    fill_group = ifelse(grepl("Immune", CellType, ignore.case = TRUE),
+                        "Immune", "Other")
+  )
+
+# Remove cells with NA CellType
+seg_annot_filtered <- seg_annot %>%
+  filter(!is.na(CellType))
+
+# Make CellType a factor with the same order as custom_colors
+seg_annot_filtered$CellType <- factor(seg_annot_filtered$CellType, levels = names(custom_colors))
+
+# Plot all cell types with custom colors
+p_all <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = CellType), color = NA) +
+  scale_fill_manual(values = custom_colors) +
+  theme_void() +
+  labs(title = "All Cell Types") +
+  guides(fill = guide_legend(ncol = 1))
+
+# Display plot
+p_all
+
+
+
+# -----------------------------
+# Basal Keratinocytes
+# -----------------------------
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Basal_epidermal",
+                        "Basal_epidermal", "Other")
+  )
+
+Basal <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Basal_epidermal" = "#6495ED", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = "Basal epidermal (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Basal
+
+# -----------------------------
+# Suprabasal Keratinocytes
+# -----------------------------
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Suprabasal_keratinocytes",
+                        "Suprabasal_keratinocytes", "Other")
+  )
+
+Suprabasal_keratinocytes <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Suprabasal_keratinocytes" = "#00ff7f", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Suprabasal_keratinocytes (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Suprabasal_keratinocytes
+
+# -----------------------------
+# Non_epidermal Keratinocytes
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Non_epidermal_keratinocytes",
+                        "Non_epidermal_keratinocytes", "Other")
+  )
+
+Non_epidermal_keratinocytes <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Non_epidermal_keratinocytes" = "#CC79A7", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Non_epidermal_keratinocytes (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Non_epidermal_keratinocytes
+
+# -----------------------------
+# Melanocytes
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Melanocytes",
+                        "Melanocytes", "Other")
+  )
+
+Melanocytes <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Melanocytes" = "#FF1493", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Melanocytes (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Melanocytes
+
+# -----------------------------
+# Sebocytes 
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Sebocytes",
+                        "Sebocytes", "Other")
+  )
+
+Sebocytes <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Sebocytes" = "#BA55D3", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Sebocytes (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Sebocytes
+
+# -----------------------------
+# Eccrine Secretory Duct Cells
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Eccrine_secretory_duct",
+                        "Eccrine_secretory_duct", "Other")
+  )
+
+Eccrine_secretory_duct <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Eccrine_secretory_duct" = "#cd5c5c", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Eccrine_secretory_duct (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Eccrine_secretory_duct
+
+
+# -----------------------------
+# Endothelial
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Endothelial_cells",
+                        "Endothelial_cells", "Other")
+  )
+
+Endothelial_cells <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Endothelial_cells" = "#9400D3", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Endothelial_cells (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Endothelial_cells
+
+
+# -----------------------------
+# Mural
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Mural",
+                        "Mural", "Other")
+  )
+
+Mural <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Mural" = "#87CEEB", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Mural (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Mural
+
+# -----------------------------
+# Fibroblasts
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Fibroblasts",
+                        "Fibroblasts", "Other")
+  )
+
+Fibroblasts <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Fibroblasts" = "#228B22", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Fibroblasts (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Fibroblasts
+
+# -----------------------------
+# Subsetting of Fibroblasts
+# -----------------------------
+
+fibro <- subset(d1922, idents = "Fibroblasts")
+DefaultAssay(fibro) <- "Xenium"
+
+
+fibro <- SCTransform(fibro, assay = "Xenium", verbose = FALSE)
+fibro <- RunPCA(fibro, npcs = num_pcs, verbose = FALSE)
+fibro <- FindNeighbors(fibro, dims = 1:num_pcs)
+fibro <- FindClusters(fibro, resolution = 0.8)
+fibro <- RunUMAP(fibro, dims = 1:num_pcs)
+
+DimPlot(fibro, label = TRUE) + ggtitle("Fibroblast Subclusters")
+
+fibro_gene_sets <- list(
+  Fb1       = c("COL18A1", "COL23A1", "APCDD1", "PTGDS"),
+  Fb2       = c("MMP2", "COL12A1", "THBS2", "MFAP5"))
+
+# Keep only genes present in the dataset
+fibro_gene_sets_present <- lapply(fibro_gene_sets, function(x) x[x %in% rownames(fibro)])
+
+expr_mat <- GetAssayData(fibro, layer = "data")
+
+for (gene_set_name in names(fibro_gene_sets_present)) {
+  genes <- fibro_gene_sets_present[[gene_set_name]]
+  score_vector <- colMeans(expr_mat[genes, , drop = FALSE])
+  fibro@meta.data[[paste0(gene_set_name, "_score")]] <- score_vector[colnames(fibro)]
+}
+
+score_cols <- paste0(names(fibro_gene_sets_present), "_score")
+fibro$subtype_annotation <- apply(fibro@meta.data[, score_cols], 1, function(x) names(fibro_gene_sets_present)[which.max(x)])
+
+cluster_subtypes <- fibro@meta.data %>%
+  group_by(seurat_clusters) %>%
+  summarize(dominant_subtype = names(sort(table(subtype_annotation), decreasing = TRUE))[1])
+
+# Map cluster-level dominant subtype back to cells
+fibro$cluster_subtype <- cluster_subtypes$dominant_subtype[match(fibro$seurat_clusters, cluster_subtypes$seurat_clusters)]
+
+#Plotting fibroblast subcluster
+DimPlot(fibro, group.by = "cluster_subtype", label = TRUE) +
+  theme(
+    legend.position = "bottom",
+    axis.title = element_blank(),
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    plot.title = element_blank()
+  )
+
+d1922$Fibroblast_Subtype_Cluster <- NA
+d1922$Fibroblast_Subtype_Cluster[Cells(fibro)] <- fibro$cluster_subtype
+
+#---------------------------------------
+# Spatial locaiton of fibroblast subsets
+#---------------------------------------
+
+cell_annotations <- d1922@meta.data %>%
+  mutate(cell_id = sub("^sample_D1922_", "", rownames(.))) %>%
+
+  
+  select(cell_id, Fibroblast_Subtype_Cluster)
+
+# Join polygons + subtype data
+seg_annot <- seg_sf %>%
+  left_join(cell_annotations, by = "cell_id")
+
+# Create final plotting group
+seg_annot <- seg_annot %>%
+  mutate(
+    fibro_subtype = case_when(
+      Fibroblast_Subtype_Cluster == "Fb1"       ~ "Fb1",
+      Fibroblast_Subtype_Cluster == "Fb2"       ~ "Fb2",
+      TRUE ~ "Non-fibroblast"
+    )
+  )
+
+# Spatial plot
+p_fibro_subtypes <- ggplot(seg_annot) +
+  geom_sf(aes(fill = fibro_subtype), color = NA) +
+  scale_y_reverse() +
+  scale_fill_manual(values = c(
+    "Fb1"       = "green",
+    "Fb2"       = "red",
+    "Non-fibroblast"  = "grey80"
+  )) +
+  theme_void() +
+  labs(title = "Fibroblast Subtypes in Spatial Context") +
+  guides(fill = guide_legend(ncol = 1))
+
+p_fibro_subtypes
+
+# -----------------------------
+# Immune
+# -----------------------------
+
+seg_annot_filtered <- seg_annot_filtered %>%
+  mutate(
+    fill_basal = ifelse(CellType == "Immune",
+                        "Immune", "Other")
+  )
+
+Immune <- ggplot(seg_annot_filtered) +
+  geom_sf(aes(fill = fill_basal), color = NA) +
+  scale_fill_manual(values = c("Immune" = "#FFD700", "Other" = "grey85")) +
+  theme_void() +
+  labs(title = " Immune (highlighted)") +
+  guides(fill = guide_legend(ncol = 1))
+
+Immune
